cedc46cff39bbac965cd5a656149fe276598ac94
Sergio Schmilovich <sergio.schmilovich@gmail.com>
2022-08-24 16:05:11 -0500

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir -p logs
+find logs -type f -mtime +30 -delete # delete files older than 30 days
 cd /home/sergio/scripts/smartfit/
 . ./env/bin/activate
 /home/sergio/scripts/smartfit/env/bin/python3 /home/sergio/scripts/smartfit/main.py >> logs/smartfit-"`date +"%Y-%m-%d_%H.%M.%S"`".log 2>&1
